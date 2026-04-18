cfb665991987e8a58a82cbccbb17648a7d4cb349
Christopher Toth <q.alpha@gmail.com>
2026-04-18 14:48:36 -0600

    Add probabilistic argumentation kernel

--- src/argumentation/__init__.py
+++ src/argumentation/__init__.py
@@ -1,5 +1,13 @@
 """Finite formal argumentation objects and algorithms."""
 
-from argumentation import af_revision, aspic, bipolar, dung, partial_af, preference
+from argumentation import af_revision, aspic, bipolar, dung, partial_af, preference, probabilistic
 
-__all__ = ["af_revision", "aspic", "bipolar", "dung", "partial_af", "preference"]
+__all__ = [
+    "af_revision",
+    "aspic",
+    "bipolar",
+    "dung",
+    "partial_af",
+    "preference",
+    "probabilistic",
+]
