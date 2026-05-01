97bceef0433bc2b61084e60eb3a955cdb91d9dce
Christopher Toth <q.alpha@gmail.com>
2026-04-30 19:07:41 -0600

    Export clingo solver adapter

--- src/argumentation/solver_adapters/__init__.py
+++ src/argumentation/solver_adapters/__init__.py
@@ -1,5 +1,5 @@
 """Optional subprocess adapters for external argumentation solvers."""
 
-from argumentation.solver_adapters import iccma_aba, iccma_af
+from argumentation.solver_adapters import clingo, iccma_aba, iccma_af
 
-__all__ = ["iccma_aba", "iccma_af"]
+__all__ = ["clingo", "iccma_aba", "iccma_af"]
